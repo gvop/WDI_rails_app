a4a2af0dbc0113241b670612c9a5e9e33f3524dd
gvop <guusvanooijen@gmail.com>
2015-11-02 14:58:50 +0000

    going to add the like function

--- sneakershowoff/app/models/sneaker.rb
+++ sneakershowoff/app/models/sneaker.rb
@@ -2,4 +2,7 @@ class Sneaker < ActiveRecord::Base
   belongs_to :user
   belongs_to :make
   mount_uploader :image, ImageUploader
+
+  default_scope -> { order(created_at: :asc) }
+  scope :reversed, -> { order(created_at: :asc) }
 end

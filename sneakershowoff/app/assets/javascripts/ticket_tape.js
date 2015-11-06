9cee2f7cff199414ee964853431eee19fae7d7a8
gvop <guusvanooijen@gmail.com>
2015-11-06 10:11:37 +0000

    Finished for presentation

--- sneakershowoff/app/assets/javascripts/ticket_tape.js
+++ sneakershowoff/app/assets/javascripts/ticket_tape.js
@@ -1,1 +1,34 @@
-console.log("test")
+$( document ).ready(function() {
+
+  var ticker        = $("#ticker")
+  var tickerItems   = $(".ticker_items")
+
+  var newTicker     = $("#ticker_jq")
+  var lengthFootage = tickerItems.length
+
+  var count = 0
+
+  ticker.hide()
+
+  if( tickerItems.length > 0 ){
+      var i = 0;
+      var fn = function(){
+          console.log(tickerItems[i])
+          newTicker.hide()
+          newTicker.html(tickerItems[i])
+          newTicker.fadeIn()
+          if( ++i < tickerItems.length ){
+              setTimeout(fn, 5000);
+          }
+      };
+      fn();
+  }
+
+})
+
+
+
+
+
+
+
--- sneakershowoff/app/controllers/makes_controller.rb
+++ sneakershowoff/app/controllers/makes_controller.rb
@@ -7,6 +7,7 @@ class MakesController < ApplicationController
   end
 
   def show
+
   end
 
   def new
